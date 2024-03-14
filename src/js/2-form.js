518735521fb1c7350412ed18ead62a6053247ae6
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2024-03-14 17:46:27 +0200

    add

---
+++ src/js/2-form.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+const localStorageKey = "feedback-form-state";
+
+
+const saveFormData = () => {
+  const formData = {
+    email: emailInput.value.trim(),
+    message: messageTextarea.value.trim()
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+};
+
+
+const loadFormData = () => {
+  const savedData = localStorage.getItem(localStorageKey);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  }
+};
+
+
+const clearFormData = () => {
+  localStorage.removeItem(localStorageKey);
+};
+
+
+form.addEventListener("input", saveFormData);
+
+
+window.addEventListener("DOMContentLoaded", loadFormData);
+
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const formData = {
+    email: emailInput.value.trim(),
+    message: messageTextarea.value.trim()
+  };
+  if (formData.email && formData.message) {
+    console.log(formData);
+    clearFormData();
+    form.reset();
+  } else {
+    console.log("Please fill in all fields.");
+  }
+});
