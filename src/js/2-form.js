0f979dac39d7bcb064d1c152e10d9ff6e62014a4
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2024-12-15 01:46:48 +0200

    g

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,50 +5,34 @@ const messageTextarea = form.querySelector('textarea[name="message"]');
 const localStorageKey = "feedback-form-state";
 
 
-const saveFormData = () => {
-  const formData = {
-    email: emailInput.value.trim(),
-    message: messageTextarea.value.trim()
-  };
+
+
+form.addEventListener("input", handleInput);
+
+function handleInput(event) {
+  
+  formData =
+{
+  email: event.target.emailInput.value,
+  message: event.target.messageTextarea.value
+};
+
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
-};
+
+}
 
 
-const loadFormData = () => {
-  const savedData = localStorage.getItem(localStorageKey);
-  if (savedData) {
-    const formData = JSON.parse(savedData);
-    emailInput.value = formData.email;
-    messageTextarea.value = formData.message;
+function loadPage() {
+
+  if (localStorage.getItem(localStorageKey, JSON.stringify(formData))) {
+
+    const formData = emailInput.event.target.email.value;
+
+
   }
-};
 
+  formData = emailInput === "" && messageTextarea === "";
 
-const clearFormData = () => {
-  localStorage.removeItem(localStorageKey);
-};
-
-
-form.addEventListener("input", saveFormData);
-
-
-window.addEventListener("DOMContentLoaded", loadFormData);
-
-
-form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const formData = {
-    email: emailInput.value.trim(),
-    message: messageTextarea.value.trim()
-  };
-  if (formData.email && formData.message) {
-    console.log(formData);
-    clearFormData();
-    form.reset();
-  } else {
-    console.log("Please fill in all fields.");
-  }
-});
-
+}
 
 
